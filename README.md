ad0d9c7933d65d3e94a83684c75c9619f17472ea
Aham Kingsley <kingsleyaham6@gmail.com>
2023-01-20 10:25:20 +0100

    todo app completed

---
+++ README.md
@@ -0,0 +1,74 @@
+# Todo Console App
+
+This is a working solution of a todo console app. is has the complete functionality of a todo app. A user can add new todo, view todos, delete todo, mark todo completed and also clear completed todos.
+
+![Screenshot](./assets/screenshot.png)
+
+## Usage
+
+- clone the repo and cd into the directory such that you are on `Todo Console App`
+- To run the solution, make sure you have [nodejs](https://nodejs.org) installed
+- use the following command terminal
+
+```bash
+npm start
+or
+node index.js
+```
+
+**_Note: To add a new task to to todos just type in the name of the task and press enter_**
+
+## List of Commands
+
+- **`-exit`** : terminate or exit application
+- **`-show`** : list todos
+- **`todoName-`** : deletes the todo
+- **`completed-`** : clear completed todos
+- **`completed todoName`** : mark the selected todo completed
+
+Watch a sample below
+
+```bash
+➜  Todo Console App:(main) $ node index
+Welcome to a Todo Console App with Complete Todo Functionality
+Commands: '-exit', '-show', 'todoName-', 'completed TodoName', 'completed-'
+
+Please enter a list item to add or a command to perform: wash plate
+Please enter a list item to add or a command to perform: watch television
+Please enter a list item to add or a command to perform: wash cloth
+Please enter a list item to add or a command to perform: completed wash plate
+
+'Wash Plate' marked completed
+
+Please enter a list item to add or a command to perform: -show
+***********TODO LIST***********
+Wash Plate  |  Completed
+Watch Television  |  Active
+Wash Cloth  |  Active
+-------------------------------
+
+Please enter a list item to add or a command to perform: -view
+command does not exist
+Commands: '-exit', '-show', 'todoName-', 'completed TodoName', 'completed-'
+
+Please enter a list item to add or a command to perform: programming
+Please enter a list item to add or a command to perform: completed programming
+
+'Programming' marked completed
+
+Please enter a list item to add or a command to perform: completed-
+
+completed todos cleared successfully
+
+Please enter a list item to add or a command to perform: -show
+
+***********TODO LIST***********
+Watch Television  |  Active
+Wash Cloth  |  Active
+-------------------------------
+
+Please enter a list item to add or a command to perform: -exit
+
+***Thanks for using our our todo app. We hope to see you next time***
+
+```
---
+++ index.js
@@ -0,0 +1,227 @@
+let todos = [];
+
+const commandLine = require("readline");
+const prompts = commandLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+printWelcomeMessage();
+questionPrompt();
+
+function questionPrompt() {
+  prompts.question(
+    "Please enter a list item to add or a command to perform: ",
+    (str = "") => {
+      str = str.trim().toLowerCase();
+
+      if (!str.includes("-") && !str.includes("completed")) {
+        if (addTodo(str)) {
+          questionPrompt();
+          return;
+        }
+        print("Task already exist in todos\n");
+        questionPrompt();
+        return;
+      }
+
+      runCommand(str);
+    }
+  );
+}
+
+function runCommand(command = "") {
+  const completedSingleTodoCommand = command.slice(0, 10) === "completed ";
+  const removeTodoCommand = command.slice(command.length - 1) === "-";
+
+  if (command === "-show") {
+    viewTodos();
+    questionPrompt();
+    return;
+  }
+
+  if (command === "-exit") {
+    exitProcess();
+    return;
+  }
+
+  if (command === "completed-") {
+    clearCompletedTodos();
+    print("completed todos cleared successfully");
+    questionPrompt();
+    return;
+  }
+
+  if (removeTodoCommand) {
+    if (removeTodo(getTodoIndex(command.slice(0, command.length - 1)))) {
+      print(
+        `'${titleCase(
+          command.slice(0, command.length - 1)
+        )}' removed successfully`
+      );
+      questionPrompt();
+      return;
+    }
+
+    print(
+      `'${titleCase(
+        command.slice(0, command.length - 1)
+      )}' does not exist in todos`
+    );
+    questionPrompt();
+    return;
+  }
+
+  if (completedSingleTodoCommand && !removeTodoCommand) {
+    if (markTodoCompleted(getTodoIndex(command.slice(10)))) {
+      print(`'${titleCase(command.slice(10))}' marked completed`);
+      questionPrompt();
+      return;
+    }
+
+    print(`'${titleCase(command.slice(10))}' does not exist in todos`);
+    questionPrompt();
+    return;
+  }
+
+  print("command does not exist");
+  printCommandList();
+  questionPrompt();
+  return;
+}
+
+// ---------------------------------------------------//
+// Todo Actions
+
+function addTodo(todo = "") {
+  if (!todoExists(todo)) {
+    const newTodo = { name: todo.toLowerCase(), completed: false };
+    todos.push(newTodo);
+    return true;
+  }
+
+  return false;
+}
+
+function removeTodo(todoIndex = -1) {
+  if (todoIndex >= 0) {
+    const remainingTodos = todos.filter((todo, index) => index !== todoIndex);
+    setTodo(remainingTodos);
+    return true;
+  }
+
+  return false;
+}
+
+function viewTodos() {
+  print("***********TODO LIST***********");
+
+  todos.forEach((todo) => {
+    print(`${titleCase(todo.name)}  |  ${getTodoStatus(todo.completed)}`);
+  });
+
+  print("-------------------------------");
+}
+
+function markTodoCompleted(todoIndex = -1) {
+  if (todoIndex >= 0) {
+    const updatedTodo = todos.map((todo, index) => {
+      if (index === todoIndex) {
+        return { ...todo, completed: true };
+      }
+
+      return todo;
+    });
+    setTodo(updatedTodo);
+
+    return true;
+  }
+
+  return false;
+}
+
+function clearCompletedTodos() {
+  const activeTodos = todos.filter((todo) => todo.completed !== true);
+  setTodo(activeTodos);
+}
+
+function setTodo(updatedTodo = []) {
+  todos = updatedTodo;
+}
+
+function getTodoStatus(status = false) {
+  switch (status) {
+    case false:
+      return "Active";
+    case true:
+      return "Completed";
+    default:
+      break;
+  }
+}
+
+function getTodoIndex(todoName = "") {
+  todoName = todoName.toLowerCase();
+  let todoIndex = -1;
+
+  todos.forEach((todo, index) => {
+    if (todo.name.toLowerCase() === todoName) {
+      todoIndex = index;
+    }
+  });
+
+  return todoIndex;
+}
+
+function todoExists(todoName = "") {
+  const todoIndex = getTodoIndex(todoName);
+  if (todoIndex >= 0) {
+    return true;
+  }
+
+  return false;
+}
+
+// ---------------------------------------------------//
+
+function printCommandList() {
+  const commands = [
+    "'-exit'",
+    "'-show'",
+    "'todoName-'",
+    "'completed TodoName'",
+    "'completed-'",
+  ];
+
+  print(`Commands: ${commands.join(", ")}\n`);
+}
+
+function printWelcomeMessage() {
+  print("Welcome to a Todo Console App with Complete Todo Functionality");
+  printCommandList();
+}
+
+function exitProcess() {
+  print(
+    "\n***Thanks for using our our todo app. We hope to see you next time***\n"
+  );
+  process.exit();
+}
+
+// ---------------------------------------------------//
+// utility functions
+
+function print(message = "") {
+  console.log(message);
+}
+
+function titleCase(str = "") {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}
+// ---------------------------------------------------//
